8d71927214e7e855425723545b18e8d747f3c78b
Yayackie <23030393@itesa.edu.mx>
2025-03-12 23:27:31 -0600

    Segunda prueba

--- src/trabajos_metodosnumericos/Trabajos_MetodosNumericos.java
+++ src/trabajos_metodosnumericos/Trabajos_MetodosNumericos.java
@@ -4,6 +4,9 @@ package trabajos_metodosnumericos;
 public class Trabajos_MetodosNumericos {
 
     public static void main(String[] args) {
+        //1.) Identificar diagonal
+        //2.) Elegir Pivote
+        //2.) Aplicar formula: f(n) - pivote
 
     }
     
